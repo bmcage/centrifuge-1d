a7c04ec32d1c40841f9554eeb84f7ca8b5b4f3c5
Pavol Kišon <pavol.kison@gmail.com>
2011-11-21 15:26:20 +0100

    rewrite direct-saturated (add method for computing characteristics, plot GC & RM, separated code for solving direct problem, added method for extracting characteristics from general z-vector of values)

--- direct-saturated.py
+++ direct-saturated.py
@@ -20,7 +20,6 @@
 from sys import argv as sysargv, path as syspath
 from os.path import join as join_path
 from config import DEFAULT_PARAMETERS, ConfigManager
-from numpy import arange
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,37 +27,77 @@ from centrifugeparameters import CentrifugeParameters
 from auxiliaryfunctions   import parse_centrifuge_input
 
 syspath.append(join_path('.', 'odes', 'build', 'lib.linux-x86_64-3.2'))
-#/scikits/sundials_odes/build//
-#print('syspath: ', syspath)
-#import ida
 
+import scikits.odes.sundials.ida as ida
 from scikits.odes.sundials.common_defs import ResFunction
+
+mass_in_idx  = 0
+mass_out_idx = 1
 
 [inifiles, outputdir, savecfgname] = \
     parse_centrifuge_input(sysargv)
     
-print('savecfgname: ', savecfgname)
+def draw_graphs(fignum, t, wl_in, wl_out, GC, RM):
+    plt.figure(fignum, figsize=(12, 8))
 
-first_idx    = 0
-last_idx     = -1
-mass_in_idx  = -1
-s1_idx       = -1
-s2_idx       = -1
-mass_out_idx = -1
-pq_idx       = -1
-
-def draw_graphs(fignum, t, wl_in, wl_out):
-    plt.figure(fignum)
-    plt.subplot(211)
+    plt.subplot(221)
     plt.plot(t, wl_in, 'b')
     plt.xlabel('Time')
-    plt.ylabel('water in inflow chamber')
+    plt.ylabel('Water in inflow chamber [cm]')
 
-    plt.subplot(212)
+    plt.subplot(222)
     plt.plot(t, wl_out, 'k')
     plt.xlabel('Time')
-    plt.ylabel('water in outflow chamber')
+    plt.ylabel('Water in outflow chamber [cm]')
+
+    plt.subplot(223)
+    plt.plot(t, wl_in, 'b')
+    plt.xlabel('Time')
+    plt.ylabel('Gravitational center [$cm$]')
+
+    plt.subplot(224)
+    plt.plot(t, wl_out, 'k')
+    plt.xlabel('Time')
+    plt.ylabel('Rotational momentum [$kg.m.s^{-2}]')
+
     plt.show()
+
+def characteristics(t, mass_in, mass_out, model):
+    porosity = model.porosity
+    r0 = model.r0
+    L  = model.l
+    l0_out = L + model.l0_out
+    l_out  = L + model.l0_out - mass_out
+
+    omega2g = (np.power(model.omega_start  + (model.omega - model.omega_start)
+                        * (1 - np.exp(-model.omega_gamma*t)), 2)
+                / model.g)
+
+    GC = np.empty(t.shape, float)
+    RM = np.empty(t.shape, float)
+
+    P = np.pi * model.d / 4
+
+    WM = P * (mass_in + porosity * L + mass_out)
+
+    for i in range(len(t)):
+        # Gravitational center
+        gc_sat   = (1/2 * model.density
+                    * (porosity * (np.power(r0 + L, 2) - np.power(r0, 2))
+                       + (np.power(r0, 2) - np.power(r0 - mass_in[i], 2))
+                       + (np.power(r0 + l0_out, 2) - np.power(r0 + l_out[i], 2))))
+        GC[i] =  P * gc_sat / WM[i]
+
+        # Rotational momentum
+        rm_sat   = (1/6 * model.density
+                    * (porosity * (np.power(r0 + L, 3) - np.power(r0, 3))
+                        + (np.power(r0, 3) - np.power(r0 - mass_in[i], 3))
+                        + (np.power(r0 + l0_out, 3) - np.power(r0 + l_out[i], 3))))
+
+        RM[i] = omega2g[i] * P * rm_sat
+
+    return GC, RM, WM
+
 
 class centrifuge_rhs(ResFunction):
     def evaluate(self, t, x, xdot, result, model):
@@ -69,28 +108,24 @@ class centrifuge_rhs(ResFunction):
         L = model.l
         l = x[0]
         q_out = model.ks*omega2g * (l*(2*model.r0 - l)/L + 2*model.r0 + L)
-        result[0] = xdot[0] + q_out
-        result[1] = xdot[1] - q_out
+        result[mass_in_idx] = xdot[0] + q_out
+        result[mass_out_idx] = xdot[1] - q_out
         
         return 0
-        
-rhs = centrifuge_rhs()     
 
-def main():
-    cfgmngr = ConfigManager.get_instance()
-    model   = cfgmngr.read_configs(merge_output_p = True,
-                                   preserve_sections_p = False,
-                                   filenames = inifiles,
-                                   defaults = [DEFAULT_PARAMETERS],
-                                   saveconfigname = savecfgname)
-    model.register_key('experiment', 'tspan', \
-                   arange(model.t_start, model.t_end, model.t_step))
-    print(model.tspan)
-    #try:
-    from scikits.odes.sundials import ida
-    #except ImportError:
-   #     print('ImportError: Could not load module ''ida''. Exiting...')
-   #     return
+
+def extract_saturated_characteristics(t, z, model):
+    GC, RM = characteristics(model.tspan, z[:, mass_in_idx],
+                             z[:, mass_out_idx], model)[:2]
+
+    return GC, RM
+
+rhs = centrifuge_rhs()
+
+def direct_saturated_problem(Ks, model):
+
+    model.ks = Ks
+
     solver = ida.IDA(rhs,
                      compute_initcond='yp0',
                      first_step=1e-18,
@@ -98,9 +133,27 @@ def main():
                      user_data=model)
     z0  = np.array([model.l0_in, 0])
     zp0 = np.zeros(z0.shape, float)
-    _flag, t, z = solver.run_solver(model.tspan, z0, zp0)[:3]
-    print(z)
-    draw_graphs(1, t, z[:, 0], z[:, 1])
+    flag, t, z = solver.run_solver(model.tspan, z0, zp0)[:3]
+
+    return flag, t, z
+
+def solve_direct(draw_graphs_p = False):
+    cfgmngr = ConfigManager.get_instance()
+    model   = cfgmngr.read_configs(merge_output_p = True,
+                                   preserve_sections_p = False,
+                                   filenames = inifiles,
+                                   defaults = [DEFAULT_PARAMETERS],
+                                   saveconfigname = savecfgname)
+    model.register_key('experiment', 'tspan',
+                   np.arange(model.t_start, model.t_end, model.t_step))
+
+    _flag, t, z = direct_saturated_problem(model.ks, model)
+
+    if draw_graphs_p:
+        GC, RM = extract_saturated_characteristics(t, z, model)
+        draw_graphs(1, t, z[:, 0], z[:, 1], GC, RM)
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    solve_direct(draw_graphs_p = True)
