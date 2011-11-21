b0bd2cc87a0d01571faa53e8b8bd19caf704bc5a
Pavol Kišon <pavol.kison@gmail.com>
2011-11-21 12:58:34 +0100

    add global characteristics (water mass, gravitational center, rotational momentum)

--- shared_functions.py
+++ shared_functions.py
@@ -57,3 +57,60 @@ def u2h(u, n, m, gamma, h = None):
         h    =  np.power(np.power(u, -1/m) - 1, 1/n) / gamma
 
     return h
+
+def water_mass(u, mass_in, mass_out, s1, s2, model):
+    ds = s2 - s1
+    dy = model.dy
+
+    V0 =  (ds/2  * (dy[0]* u[:, 0] + dy[-1]*u[:, -1] + np.sum((dy[:-1] + dy[1:])*u[:, 1:-1], 1)))
+
+    V = model.density * (mass_in + model.porosity*V0 + mass_out)
+
+    return V
+
+def characteristics(t, u, mass_in, mass_out, s1, s2, model):
+    porosity = model.porosity
+    y  = model.y
+    dy = model.dy
+    r0 = model.r0
+    L  = model.l
+    l0_out = L + model.l0_out
+    l_out  = L + model.l0_out - mass_out
+
+    ds = s2 - s1
+
+    omega2g = (np.power(model.omega_start  + (model.omega - model.omega_start)
+                                  * (1 - np.exp(-model.omega_gamma*t)), 2)
+                / model.g)
+
+    GC = np.empty(t.shape, float)
+    RM = np.empty(t.shape, float)
+    WM = water_mass(u, mass_in, mass_out, s1, s2, model)
+
+    P = np.pi * model.d / 4
+
+    for i in range(len(t)):
+        # Gravitational center
+        gc_unsat = (porosity * 1/2 * model.density * ds[i]
+                    * ((r0 + s1[i])*dy[0]*u[i, 0] + (r0 + s2[i])*dy[-1]*u[i, -1]
+                        + np.sum((dy[:-1]+dy[1:])*(r0 + s1[i] + ds[i]*y[1:-1])*u[i, 1:-1])))
+        gc_sat   = (1/2 * model.density
+                    * (porosity * (np.power(r0 + s1[i], 2) - np.power(r0, 2))
+                       + (np.power(r0, 2) - np.power(r0 - mass_in[i], 2))
+                       + (np.power(r0 + l0_out, 2) - np.power(r0 + l_out[i], 2))))
+        GC[i] =  P * (gc_unsat + gc_sat) / WM[i]
+
+        # Rotational momentum
+        rm_unsat = (porosity * 1/4 * model.density * ds[i]
+                    * (np.power(r0 + s1[i], 2)*dy[0]*u[i, 0]
+                       + np.power(r0 + s2[i], 2)*dy[-1]*u[i, -1]
+                       + np.sum((dy[:-1]+dy[1:]) * u[i, 1:-1]
+                                 * np.power(r0 + s1[i] + ds[i]*y[1:-1], 2))))
+        rm_sat   = (1/6 * model.density
+                    * (porosity * (np.power(r0 + s1[i], 3) - np.power(r0, 3))
+                        + (np.power(r0, 3) - np.power(r0 - mass_in[i], 3))
+                        + (np.power(r0 + l0_out, 3) - np.power(r0 + l_out[i], 3))))
+
+        RM[i] = omega2g[i] * P * (rm_unsat + rm_sat)
+
+    return GC, RM, WM
