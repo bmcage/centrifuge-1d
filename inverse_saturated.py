d41f5ce897902c34e14c0ba25b7838da1183131b
Pavol Kišon <pavol.kison@gmail.com>
2011-11-24 15:30:43 +0100

    inverse_saturated: add inverse methods for solving "water heights" data

--- inverse_saturated.py
+++ inverse_saturated.py
@@ -30,7 +30,7 @@ from auxiliaryfunctions   import parse_centrifuge_input
 syspath.append(join_path('.', 'odes', 'build', 'lib.linux-x86_64-3.2'))
 
 from auxiliaryfunctions import load_data
-from direct_saturated import direct_saturated_problem, extract_saturated_characteristics
+from direct_saturated import direct_saturated_problem, extract_saturated_characteristics, extract_saturated_water_heights
 
 mass_in_idx  = 0
 mass_out_idx = 1
@@ -38,12 +38,28 @@ mass_out_idx = 1
 [inifiles, outputdir, savecfgname] = \
     parse_centrifuge_input(sysargv)
 
-def inverse_saturated(model, Ks):
+def inverse_saturated_characteristics(model, Ks):
     model.ks = Ks
     print(Ks)
     _flag, t, z = direct_saturated_problem(model)
     GC, RM = extract_saturated_characteristics(t, z, model)
     return np.concatenate((GC, RM))
+
+def inverse_saturated_heights(xdata, Ks):
+    model    = xdata[0]
+    heights0 = xdata[1]
+    model.ks = Ks
+    print(Ks)
+    #print(xdata)
+    heights = np.empty(heights0.shape, float)
+    for i in np.arange(len(heights0)):
+        model.l0_in = heights0[i]
+        _flag, t, z = direct_saturated_problem(model)
+        h1 = extract_saturated_water_heights(z, model)
+        print(h1)
+        heights[i] = h1[1]
+
+    return heights
 
 def main():
     cfgmngr = ConfigManager.get_instance()
@@ -58,20 +74,42 @@ def main():
         raise ValueError('Data file for inverse problem not specified !')
 
     data = load_data(model.inverse_data_filename)
-    t_measured  = data['t']
-    GC_measured = data['GC']
-    RM_measured = data['RM']
-    model.tspan = t_measured
-    data_measured = np.concatenate((GC_measured, RM_measured))
+
+    t_measured    = data['t']
+    model.tspan   = t_measured
+
+    if model.inverse_data_type == 0:
+        GC_measured   = data['GC']
+        RM_measured   = data['RM']
+        data_measured = np.concatenate((GC_measured, RM_measured))
+        xdata         = model
+        inverse_fn    = inverse_saturated_characteristics
+    elif model.inverse_data_type == 1:
+        heights_0     = data['heights0']
+        heights_1     = data['heights1']
+        data_measured = heights_1
+        xdata         = (model, heights_0)
+        inverse_fn    = inverse_saturated_heights
+    else:
+        raise ValueError('Unrecognized inverse_data_type: %d'
+                         % model.inverse_data_type)
+        
     data.close()
 
+    #print(heights_0.dtype, heights_1.dtype, t_measured.dtype)
     Ks_init = model.ks
-    measurements = np.concatenate((GC_measured, RM_measured))
-    Ks_inv, cov_ks = curve_fit(inverse_saturated, model, data_measured, p0 = Ks_init)
+    Ks_inv, cov_ks = curve_fit(inverse_fn, xdata,
+                               data_measured, p0 = Ks_init)
 
     print('Ks initial: ', Ks_init)
     print('Ks found:   ', Ks_inv)
-    print('Ks exact:   ', 2.4e-5)
+    if model.inverse_data_type == 0:
+        print('Ks exact:   ', 2.4e-5)
+    elif model.inverse_data_type == 1:
+        h1_inv = inverse_fn(xdata, Ks_inv)
+        print('measured value - computed value')
+        for i in np.arange(len(heights_0)):
+            print('% f - % f', heights_1[i], h1_inv[i])
 
 if __name__ == "__main__":
     main()
