7a07d977fa043b446e9c06bcc520031f23a96f8d
Pavol Kišon <pavol.kison@gmail.com>
2011-12-14 11:37:09 +0100

    inverse_saturated: rename inverse functions, add for ip_saturated_heights possible t_end return value, improve inverse output readability and add error estimation

--- inverse_saturated.py
+++ inverse_saturated.py
@@ -17,7 +17,7 @@ mass_out_idx = 1
 
 
 
-def inverse_saturated_characteristics(model, Ks):
+def ip_direct_saturated_characteristics(model, Ks):
     model.ks = Ks
     if model.debugging:
         print(Ks)
@@ -25,25 +25,30 @@ def inverse_saturated_characteristics(model, Ks):
     GC, RM = extract_saturated_characteristics(t, z, model)
     return np.concatenate((GC, RM))
 
-def inverse_saturated_heights(xdata, Ks):
-    model    = xdata[0]
-    heights0 = xdata[1]
+def ip_direct_saturated_heights(xdata, Ks):
+    model       = xdata[0]
+    heights0    = xdata[1]
+    return_time = xdata[2]
     model.ks = Ks
 
     heights = np.empty(heights0.shape, float)
+    t_end   = np.empty(heights0.shape, float)
     for i in np.arange(len(heights0)):
         model.l0_in = heights0[i]
         _flag, t, z = solve_direct_saturated_problem(model)
 
         h1 = extract_saturated_water_heights(z, model)
         heights[i] = h1[1]
+        t_end[i]   = t[1] # t = [start_time, end_time]
 
         if model.debugging:
             print('t = ', t, ', Ks =  ', Ks, ', h0 = ', model.l0_in,
                   ', h_found = ', heights[i], ', r0 = ', model.r0,
                   'omega = ', model.omega, ' flag = ', _flag)
-
-    return heights
+    if return_time:
+        return heights, t_end
+    else:
+        return heights
 
 def solve_inverse_saturated(model, measured_data_filename):
     data = load_measured_data(measured_data_filename)
@@ -56,38 +61,45 @@ def solve_inverse_saturated(model, measured_data_filename):
     model.omega    = data.omega
     model.l        = data.length
 
-    print(model.tspan, t_duration, t_deceleration, model.t_end)
-
     if model.data_type == 0:
         raise NotImplemented('inverse::characteristics: load characteristics data not implemented !')
         GC_measured   = data['GC']
         RM_measured   = data['RM']
         data_measured = np.concatenate((GC_measured, RM_measured))
         xdata         = model
-        inverse_fn    = inverse_saturated_characteristics
+        direct_fn    = ip_direct_saturated_characteristics
     elif model.data_type == 1:
         heights_0     = np.asarray(data.h0, float)
         heights_1     = np.asarray(data.h1, float)
         data_measured = heights_1
-        xdata         = (model, heights_0)
+        return_time   = False
+        xdata         = (model, heights_0, return_time)
 
-        inverse_fn = inverse_saturated_heights
+        direct_fn = ip_direct_saturated_heights
     else:
         raise ValueError('Unrecognized data_type: %d'
                          % model.data_type)
 
     Ks_init = model.ks
      
-    Ks_inv, cov_ks = curve_fit(inverse_fn, xdata,
+    Ks_inv, cov_ks = curve_fit(direct_fn, xdata,
                                data_measured, p0 = Ks_init)
 
-    h1_inv = inverse_fn(xdata, Ks_inv)
-    if model.debugging:
-        print('measured value - computed value')
+    return_time   = True # return also times for estimating error
+    xdata         = (model, heights_0, return_time)
+    h1_inv, t_inv = direct_fn(xdata, Ks_inv)
 
     for i in np.arange(len(heights_0)):
-        print('Subexperiment ', i+1, '(measured - computed value)',
-              '% f - % f' % (heights_1[i], h1_inv[i]))
+        print('Subexperiment ', i+1)
+        print('    h0:          % .6f' % heights_0[i])
+        print('    h1_measured: % .6f    t_end_expected: % .2f' %
+              (heights_1[i],  model.tspan[1]))
+        print('    h1_computed: % .6f    t_end_computed: % .2f' %
+              (h1_inv[i], t_inv[i]))
+        print('    Error (%%):   % .2f                        % .2f' %
+              ((h1_inv[i] - heights_1[i]) / heights_1[i] * 100,
+               (t_inv[i] - model.tspan[1]) / model.t_end * 100))
+        
 
     return Ks_inv
 
@@ -121,7 +133,7 @@ def run_inverse_saturated():
         Ks_inv[i] = solve_inverse_saturated(model, data_filenames[i])
 
     Ks_inv_disp = Ks_inv / 100
-    print('Ks_inv: ')
+    print('\n\nKs_inv computed: ')
     for Ks_print in Ks_inv_disp:
         print(Ks_print)
 
