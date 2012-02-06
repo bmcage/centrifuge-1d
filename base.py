94d351e2e0d2f830aec15be3059bce426fb23386
Pavol Kišon <pavol.kison@gmail.com>
2012-02-06 14:16:33 +0100

    move ModelParameters class to base.py; remove model_parameters.py, dae.py and config_old.py

--- base.py
+++ base.py
@@ -37,3 +37,58 @@ def adjust_base_cfg(cfg, adjust_all = False, adjust_omega = False,
                                      model.t_end + model.deceleration_duration
                                                 + model.t_step / 10.,
                                      model.t_step))
+
+
+class ModelParameters:
+    """
+    Parameters of the centrifuge
+    """
+    def __init__(self, cfg = None):
+        if cfg:
+            self.register_keys(cfg)
+
+    def register_key(self, key, value):
+        if hasattr(self, key):
+            raise Exception("Atrribute '%s' already exists !" % key)
+        else:
+            setattr(self, key, value)
+
+    def register_keys(self, cfg):
+        for keys_values in cfg.values():
+            for (key, value) in keys_values.items():
+                self.register_key(key.lower(), value)
+
+    def set(self, key, value):
+        key_lower = key.lower()
+
+        if not hasattr(self, key_lower):
+            raise Exception('Atrribute ''%s'' does not exist !' % key)
+
+        value_type = type(value)
+        key_type   = type(getattr(self, key_lower))
+
+        if value_type == key_type:
+            setattr(self, key_lower, value)
+        elif value_type == int and key_type == float:
+            #convert int automatically to float
+            setattr(self, key, float(value))
+        elif value_type == list:
+            for item in value:
+                if type(item) == int and key_type == float:
+                    pass
+                elif not ((type(item) == key_type) or
+                          (type(item) == int and key_type == float)):
+                    raise ValueError("ModelParameters: key '%s' has wrong type."
+                            " Expected type '%s' and got type '%s' of %s"
+                            % (key, key_type, value_type, value))
+                if value and type(value[0] == int) and (key_type == float):
+                    value = [float(item) for item in value]
+
+                setattr(self, key, value)
+        else:
+            raise ValueError("ModelParameters: key '%s' has wrong type. Expected"
+                             " type '%s' and got type '%s' of %s"
+                             % (key, key_type, value_type, value))
+
+
+
