6197d0b7d027dabcd064e2679eabced7dee517fd
Pavol Kišon <pavol.kison@gmail.com>
2012-02-07 11:35:06 +0100

    modules: fix desc string in info files

--- modules/direct_saturated/info.py
+++ modules/direct_saturated/info.py
@@ -2,5 +2,4 @@ scheme = 'direct'
 
 types = ['dsh', 'direct-saturated-heights', 'direct_saturated_heights']
 
-desc  = "Module for computation of the direct problem for fully saturated 
-sample in which only saturated flow occurs"
+desc  = "Module for computation of the direct problem for fully saturated sample in which only saturated flow occurs"
--- modules/inverse_saturated/info.py
+++ modules/inverse_saturated/info.py
@@ -2,5 +2,4 @@ scheme = 'inverse'
 
 types = ['ish', 'inverse-saturated-heights', 'inverse_saturated_heights']
 
-desc  = "Module for solving inverse problem of finding saturated permeability
-Ks of fully saturated sample in which only saturated flow occurs"
+desc  = "Module for solving inverse problem of finding saturated permeability Ks of fully saturated sample in which only saturated flow occurs"
