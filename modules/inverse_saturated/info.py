6e40d2674fd02c62c745f09a3dc52f105c1a3dac
Pavol Kišon <pavol.kison@gmail.com>
2012-02-09 09:03:57 +0100

    inverse_saturated: add inv-f falling head test type

--- modules/inverse_saturated/info.py
+++ modules/inverse_saturated/info.py
@@ -1,5 +1,7 @@
 scheme = 'inverse'
 
-types = ['ish', 'inverse-saturated-heights', 'inverse_saturated_heights']
+types = ['ish', 'inverse-saturated-heights', 'inverse_saturated_heights',
+         'ish-f', 'inverse-saturated-heights-falling-head',
+         'inverse_saturated_heights_falling_head']
 
 desc  = "Module for solving inverse problem of finding saturated permeability Ks of fully saturated sample in which only saturated flow occurs"
