81546ba690ae7c65ed81453052946f7abdca98e3
Pavol Kišon <pavol.kison@gmail.com>
2012-02-10 17:21:18 +0100

    inverse_saturated: csv_parse - fix skip method for consolidation problem

--- modules/inverse_saturated/csv_parse.py
+++ modules/inverse_saturated/csv_parse.py
@@ -1,10 +1,9 @@
 #from modules.direct_saturated.csv_parse import filtered_fields
 
 def skip(exp_type, row, indexes):
-    if exp_type == 'ish':
+    if exp_type == 'ish-sc':
         # only those rows where the difference between the
         # starting and ending soil heigh has less than 5% difference
-
         l0 = float(row[indexes['l0']])
         l1 = float(row[indexes['l1']])
         return (abs((l0 - l1) / l0) > 0.05)
