6ffa8cdd5937213350b58c3cb47fd0328f4e9326
Pavol Kišon <pavol.kison@gmail.com>
2012-02-14 13:25:17 +0100

    common: add collector

--- common.py
+++ common.py
@@ -72,3 +72,37 @@ def load_modules_names(submodule):
             raise ValueError()
 
     return find_module
+
+def print_by_tube(tube_number, tube_data):
+    print('Tube number: ', tube_number)
+    for tdata in tube_data:
+        print(tdata[0] / 100.)
+
+def make_collector(tubes_numbers):
+    by_tube_collection = {tube_no:[] for tube_no in tubes_numbers}
+    fifo_collection = []
+
+    def collection(command, data = None, tube_no = None):
+        if command == 'collect':
+            if tube_no and data:
+                by_tube_collection[tube_no].append(data)
+            else:
+                raise ValueError('Collector:collect - tube number or data'
+                                 ' not specified')
+        elif command in ['print', 'print-by-tube', 'print-fifo']:
+            if data:
+                if command == 'print-by-tube':
+                    for (tube_no, tube_data) in by_tube_collection.items():
+                        data(tube_no, tube_data)
+                else:
+                    data(fifo_collection)
+            else:
+                raise ValueError('Collector:print data not specified. Data'
+                                 ' has to contain a print-data function.')
+        elif command == 'get':
+            return (fifo_collection, by_tube_collection)
+        else:
+            raise ValueError('Collector: Unknown command: %s.\n Valid commans'
+                             ' are: "collect", "print", "print-by-tube",'
+                             ' "print-fifo" or "get".' % command)
+    return collection
