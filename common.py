c03921d05abecb618c265dd01f253f78c5067131
Pavol Kišon <pavol.kison@gmail.com>
2012-03-31 13:28:34 +0200

    common: remove obsolete read_model() and set_model_attributes() functions

--- common.py
+++ common.py
@@ -1,31 +1,5 @@
 import numpy as np
 
-def read_model(cfg_filenames, base_cfg = {}, check_cfg_fn = None,
-               adjust_cfg_fns = None):
-    """
-    Loads centrifuge inifiles (filenames - either a file or a list of files)
-    and on the loaded data object calls the post_hook_fn (if set).
-    """
-    from config import read_cfgs, merge_cfgs, base_cfg
-    from model_parameters import ModelParameters
-
-    [cfg] = read_cfgs(inifilenames, preserve_sections_p=True, cfgs_merge=True)
-
-    model_cfg = merge_cfgs(base_cfg, cfg)
-
-    if adjust_cfg_fns:
-        for fn in adjust_cfg_fns:
-            fn(model_cfg)
-    if check_cfg:
-        check_cfg(model_cfg)
-
-    model = ModelParameters(model_cfg)
-
-    adjust_model_default(model, adjust_all = True)
-    apply_functions(post_hook_fns, model)
-
-    return model
-
 def load_modules(submodule_name):
     """
     Loads all submodules of name 'submodule_name', which are accessed by
@@ -146,14 +120,3 @@ def make_collector(tubes_numbers):
                              ' are: "collect", "print", "print-by-tube",'
                              ' "print-fifo" or "get".' % command)
     return collection
-
-def set_model_attributes(model, iteration, attributes_list):
-    for attribute in attributes_list:
-        value = getattr(model, attribute)
-
-        if not np.isscalar(value):
-            setattr(model, '_%s' % attribute, value[iteration])
-        elif iteration == 0:
-             setattr(model, '_%s' % attribute, value)
-
-    model.tspan  = np.asarray([0.0, model.duration[iteration]])
