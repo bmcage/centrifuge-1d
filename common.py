463d24789a4c6d9936dc1169e8eeb96075924075
Pavol Kišon <pavol.kison@gmail.com>
2012-03-31 13:09:43 +0200

    common: remove unused (superseded) load_modules_names() function

--- common.py
+++ common.py
@@ -10,7 +10,7 @@ def read_model(cfg_filenames, base_cfg = {}, check_cfg_fn = None,
     from model_parameters import ModelParameters
 
     [cfg] = read_cfgs(inifilenames, preserve_sections_p=True, cfgs_merge=True)
-    
+
     model_cfg = merge_cfgs(base_cfg, cfg)
 
     if adjust_cfg_fns:
@@ -94,56 +94,6 @@ def load_experiment_types():
         return types_to_names[exp_type]
 
     return find_module_name
-
-def load_modules_names(submodule):
-    """
-    Lists all experiment types defined in modules/<module_name>.info.py
-    and returns a function for quick finding the module for given
-    experiment type as input
-    """
-
-    # We keep the list of available modules in modules names, where
-    # every modules_names[exp_type] points to given module name that
-    # exports it. Modules itself are loaded only on demand.
-    existing_modules = {}
-    loaded_modules   = {}
-
-    from os import listdir
-    from sys import modules as sysmodules
-
-    modules_names = listdir('modules')
-
-    for module_name in modules_names:
-        if module_name not in ['__pycache__', '__init__.py']:
-            try:
-                module_full_name = 'modules.' + module_name + '.info'
-                __import__(module_full_name)
-
-
-                info = sysmodules[module_full_name]
-
-                for exp_id in info.types:
-                    existing_modules[exp_id] = module_name
-            except:
-                print('Module load error:Module "%s" could not be loaded.'
-                      ' Skipping.\n'  % module_name)
-
-    def find_module(exp_type):
-        if exp_type in existing_modules:
-            module_name = existing_modules[exp_type]
-
-            if not module_name in loaded_modules:
-                module_full_name = 'modules.' + module_name + '.' + submodule
-                __import__(module_full_name)
-                loaded_modules[module_name] = sysmodules[module_full_name]
-
-            return loaded_modules[module_name]
-        else:
-            print('\n\nUnknown experiment type: %s\n' % exp_type)
-            print('Available experiments are:\n %s' % modules_names)
-            raise exit(1)
-
-    return find_module
 
 def print_by_tube(tube_number, tube_data):
     print('Tube number: ', tube_number)
