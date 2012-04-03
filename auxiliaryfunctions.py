aaa9453876cf609d80dd938069bca0f730486814
Pavol Kišon <pavol.kison@gmail.com>
2012-04-03 10:25:37 +0200

    auxiliaryfunctions: remove obsolete and unused functions

--- auxiliaryfunctions.py
+++ auxiliaryfunctions.py
@@ -1,51 +1,10 @@
-import const, getopt, sys, os
+import os
 import numpy as np
-
-def parse_centrifuge_input(args):
-    """
-    Read the ini file, use default if none given on command line
-    """
-    
-    INIFILES = [const.INIFILE_DEFAULT, \
-            const.INIFILE_DEFAULT_EXT]
-    OUTPUTDIR = const.DATA_DIR
-    SAVECONFIG = ''
-
-    try:
-        options, leftargs = getopt.getopt(args[1:],
-                                          const.SHORTOPTS, const.LONGOPTS)
-    except (getopt.GetoptError, msg):
-        print(msg)
-        # return without filling anything if we could not parse the args
-        print("Error parsing the arguments: %s " % self.args[1:])
-        sys.exit(0)
-        if leftargs:
-            print('run_centrifuge.py does not understand argument %s' % leftargs)
-            sys.exit(0)
-    
-    for opt_ix in range(len(options)):
-        option, value = options[opt_ix]
-        if option in ( '-i', '--inifiles'):
-            ini_input = value.split(',')
-            if len(ini_input) == 1:
-                INIFILES[0] = ini_input[0]
-            else:
-                INIFILES = ini_input
-        elif option in ('-o', '--outputdir'):
-            OUTPUTDIR = value
-        elif option in ('-s', '--saveconfig'):
-            SAVECONFIG = value
-
-    return [INIFILES, OUTPUTDIR, SAVECONFIG]
 
 def ensure_dir(f):
     d = os.path.dirname(f)
     if not os.path.exists(d):
         os.makedirs(d)
-
-def save_data(filename, data_dict):
-    ensure_dir(filename)
-    np.savez(filename, **data_dict)
 
 def ensure_numpy_array(value, arraysize_if_single_value = 0):
     try:
@@ -68,24 +27,6 @@ def ensure_numpy_array(value, arraysize_if_single_value = 0):
             return np.asarray(value, float)
     except:
         raise ValueError('af.ensure_numpy_value: value not a number or sequence of numbers: %s' % value)
-
-def save_data_measured_saturated_heights(filename, t, h1, h2, L, omega, porosity):
-    t  = ensure_numpy_array(t)
-    h1 = ensure_numpy_array(h1)
-    h2 = ensure_numpy_array(h2)
-    L  = ensure_numpy_array(L)
-    omega = ensure_numpy_array(omega)
-    porosity = ensure_numpy_array(porosity)
-
-    save_data(filename, {'t': t,
-                         'heights0': h1, 'heights1': h2,
-                         'length': L,
-                         'omega': omega,
-                         'porosity': porosity})
-
-def load_data(filename):
-    npzfile = np.load(filename)
-    return npzfile
 
 def apply_functions(fns, *args):
     """
@@ -131,25 +72,3 @@ def compose_functions(fns, data):
             fns(data)
 
         return result
-
-def adjust_data_default(data):
-    if type(data.omega) == list:
-        data.omega = [omega * np.pi/ 30 for omega in data.omega]
-    else:
-        data.omega       = data.omega * np.pi/ 30. # (2pi)*omega/60
-
-def load_measured_data(filename, post_hook_fns = None):
-    from config import ConfigManager, DEFAULT_DATA_PARAMETERS
-
-    cm = ConfigManager.get_instance()
-    measured_data = cm.read_configs(merge_output_p = True,
-                                    preserve_sections_p = False,
-                                    filenames = [filename],
-                                    defaults = [DEFAULT_DATA_PARAMETERS],
-                                    saveconfigname = '')
-
-    adjust_data_default(measured_data)
-    apply_functions(post_hook_fns, measured_data)
-
-    return measured_data
-
