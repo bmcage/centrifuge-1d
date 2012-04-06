d254945710afbc7929641efcf55305849a1b813c
Pavol Kišon <pavol.kison@gmail.com>
2012-04-06 12:39:52 +0200

    modules.base: - move 'include_acceleration' from mandatory parameters to defaults (default = True),   it cannot be moved do dependent, as falling-head test requires its presence (should be changed !) - move 'duration' from mandatory parameters to dependent (because 'duration' is the time of centrifugation,   which is not needed in case we perform falling-head test only) - therefore also omega was removed from mandatory - add 'centrifugation' label into dependent variables, that adds 'omega' if centrifugation is performed - the rest of options were only aligned...

--- modules/base/options.py
+++ modules/base/options.py
@@ -3,19 +3,24 @@ PARENTAL_MODULES = []
 CONFIG_OPTIONS = {
         'mandatory': ['exp_type',
                       'ks',
-                      'r0', 'l0', 'omega', 'duration',
-                      'include_acceleration',
+                      'r0', 'l0', 'duration',
                       'wt_out',
                       'atol', 'rtol'],
-        'defaults': {'g': 981,
-                     'omega_start': 0.0, 'omega_end': 0.0,
-                     'ks1': -1.0, 'fl1': 0.0, 'ks2': -1.0, 'fl2': 0.0,
-                     'fh_duration': 0.0, 'r0_fall': 1e5,
-                     'density': 1.0, 'viscosity': 1.0},
-        'dependent': {'acceleration': \
-                      (lambda cfg: cfg.get_value('include_acceleration'),
-                       ['omega_start', 'omega_end', 'deceleration_duration'])},
-        'optional': ['l1', 'measured_gc', 'measured_w'],
+        'defaults':  {'g': 981,
+                      'omega_start': 0.0, 'omega_end': 0.0,
+                      'ks1': -1.0, 'fl1': 0.0, 'ks2': -1.0, 'fl2': 0.0,
+                      'fh_duration': 0.0, 'r0_fall': 1e5,
+                      'density': 1.0, 'viscosity': 1.0,
+                      'include_acceleration': False
+                     },
+        'dependent': {'centrifugation':
+                        (lambda cfg: not (cfg.get_value('duration') == 0.0),
+                         ['omega']),
+                      'acceleration':
+                        (lambda cfg: cfg.get_value('include_acceleration'),
+                         ['omega_start', 'omega_end', 'deceleration_duration'])
+                     },
+        'optional':  ['l1', 'measured_gc', 'measured_w'],
         'additional': []
         }
 
