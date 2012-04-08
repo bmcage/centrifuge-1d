efced98392d0b931d282fa0a56ae2f8ac3142bb8
Pavol Kišon <pavol.kison@gmail.com>
2012-04-08 18:56:57 +0200

    direct_draining_saturated.options: add 'm' as additional parameter

--- modules/direct_draining_saturated/options.py
+++ modules/direct_draining_saturated/options.py
@@ -9,7 +9,7 @@ CONFIG_OPTIONS = {
         'defaults'  : {},
         'dependent' : {},
         'optional'  : ['n1', 'gamma1', 'n2', 'gamma2'],
-        'additional': ['y', 'y12', 'dy', 'ldc1', 'ldc2', 'ldc3',
+        'additional': ['m', 'y', 'y12', 'dy', 'ldc1', 'ldc2', 'ldc3',
                        'first_idx', 'last_idx', 's1_idx', 's2_idx',
                        'mass_in_idx', 'mass_out_idx', 'pq_idx', 'z_size',]
         }
