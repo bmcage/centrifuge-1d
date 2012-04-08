fa1c001f6caef0050787d4492f657fac9d6c632d
Pavol Kišon <pavol.kison@gmail.com>
2012-04-08 18:56:14 +0200

    base.options: - add 'omega_fall' as additional parameter - add support for falling head test - if 'duration' == 0. we perform   only the falling head test and set as 'duration' <- 'fh_duration', 'omega' <- 'omega_fall', 'r0' <- 'r0_fall' and we disable the centrifuge acceleration

--- modules/base/options.py
+++ modules/base/options.py
@@ -22,7 +22,7 @@ CONFIG_OPTIONS = {
                          ['omega_start', 'omega_end', 'deceleration_duration'])
                      },
         'optional':  ['l1', 'measured_gc', 'measured_w'],
-        'additional': []
+        'additional': ['omega_fall']
         }
 
 EXCLUDE_FROM_MODEL = []
@@ -36,4 +36,12 @@ def adjust_cfg(cfg):
        by configuration file(s), e.g. allocate the discretized interval
        based on the discretization type and number of inner points.
     """
-    pass
+    duration = cfg.get_value('duration')
+    if (type(duration) == list) and (not any(duration)) or (duration == 0.0):
+        # We perform falling-head test only
+        cfg.set_value('duration', cfg.get_value('fh_duration'))
+        cfg.set_value('fh_duration', 0.0)
+
+        cfg.set_value('include_acceleration', False)
+        cfg.set_value('r0', cfg.get_value('r0_fall'))
+        cfg.set_value('omega', cfg.get_value('omega_fall'))
