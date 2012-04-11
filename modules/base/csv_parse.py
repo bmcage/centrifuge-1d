393125c61874e9739eec1a17a4934cf1a782cfcd
Pavol Kišon <pavol.kison@gmail.com>
2012-04-11 10:42:50 +0200

    modules.base.csv_parse: add description string to skip() function

--- modules/base/csv_parse.py
+++ modules/base/csv_parse.py
@@ -1,4 +1,9 @@
 def skip(exp_type, row, indexes):
+    """
+      Check every 'row' of a .csv file. If the row is not wanted, set return
+      flag to 'False' and row will be ommited from the resulting .ini file.
+      Otherwise set return flag to 'True'.
+    """
     return False
 
 def CSV2ini_fields(field_name, field_value):
