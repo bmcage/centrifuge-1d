3923a57e49fe3fc96692ff6da302da69d5682195
Pavol Kišon <pavol.kison@gmail.com>
2012-04-17 16:42:48 +0200

    dds:replace characteristics() with modules.shared.characteristics [namely function water_mass(), calc_gc()]     and add WM to display also in output

--- modules/direct_draining_saturated/run.py
+++ modules/direct_draining_saturated/run.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from scikits.odes.sundials.common_defs import ResFunction
 from modules.shared.shared_functions import find_omega2g, h2Kh, dudh, h2u
+from modules.shared.characteristics import water_mass, calc_gc
 from modules.shared.solver import simulate_direct
 
 class centrifuge_residual(ResFunction):
@@ -80,7 +81,7 @@ class centrifuge_residual(ResFunction):
             q_last  = np.maximum(1e-12,
                                  -Kh_last * (dhdr_last/ds - omega2g*(r0 + L)))
             result[last_idx]  = hdot[-1]
-            print('kh', Kh_last, q_last)
+            print('kh', Kh_last, q_last, dhdr_last)
 
         result[model.mass_in_idx]  = zdot[model.mass_in_idx]
         result[model.mass_out_idx] = zdot[model.mass_out_idx]  - q_last
@@ -90,78 +91,13 @@ class centrifuge_residual(ResFunction):
 
         return 0
 
-def characteristics(t, u, mass_in, mass_out, s1, s2, model, chtype = 'all'):
-
-    calc_gc = chtype in ['all', 'gc']
-    calc_rm = chtype in ['all', 'rm']
-
-    porosity = model.porosity
-    y  = model.y
-    dy = model.dy
-    L  = model.l0
-    l0_out = L + model.wt_out
-    l_out  = L + model.wt_out - mass_out
-
-    ds = s2 - s1
-
-    if calc_rm:
-        P = np.pi * model.d / 4
-        omega2g = find_omega2g(t, model.omega, model)
-
-    # Water mass
-    wm_sat = ds/2  * (dy[0]* u[0] + dy[-1]*u[-1]
-                      + np.sum((dy[:-1] + dy[1:])*u[1:-1]))
-
-    WM    = model.density * (mass_in + porosity*wm_sat + mass_out)
-
-    # GC is from the start of the sample (not from centr.axis)
-    # sample = filter1 + soil + filter2
-    r0_gc = model.fl1
-    r0_rm = model.r0
-
-     # Gravitational center
-    r0 = r0_gc
-    if calc_gc:
-        gc_unsat = (porosity * 1/2 * model.density * ds
-                    * ((r0 + s1)*dy[0]*u[0]
-                       + (r0 + s2)*dy[-1]*u[-1]
-                       + np.sum((dy[:-1]+dy[1:])
-                                *(r0 + s1 + ds*y[1:-1])*u[1:-1])))
-        gc_sat   = (1/2 * model.density
-                    * (porosity * (np.power(r0 + s1, 2) - np.power(r0, 2))
-                       + (np.power(r0, 2) - np.power(r0 - mass_in, 2))
-                       + (np.power(r0 + l0_out, 2) - np.power(r0 + l_out, 2))))
-        gc_left  = (gc_unsat + gc_sat) / WM
-        gc_right = model.fl1 + model.l0 + model.fl2 - gc_left
-        GC       = gc_right
-    else:
-        GC = None
-
-    # Rotational momentum
-    if calc_rm:
-        r0 = r0_rm
-        rm_unsat = (porosity * 1/4 * model.density * ds
-                    * (np.power(r0 + s1, 2)*dy[0]*u[0]
-                       + np.power(r0 + s2, 2)*dy[-1]*u[-1]
-                       + np.sum((dy[:-1]+dy[1:]) * u[1:-1]
-                                * np.power(r0 + s1 + ds*y[1:-1], 2))))
-        rm_sat   = (1/6 * model.density
-                    * (porosity * (np.power(r0 + s1, 3) - np.power(r0, 3))
-                       + (np.power(r0, 3) - np.power(r0 - mass_in, 3))
-                       + (np.power(r0 + l0_out, 3) - np.power(r0 + l_out, 3))))
-
-        RM = omega2g * P * (rm_unsat + rm_sat)
-    else:
-        RM = None
-
-    return GC, RM, WM
-
 residual_fn = centrifuge_residual()
 
 def solve(model):
 
     t   = np.empty([model.iterations+1, ], dtype=float)
     GC  = np.empty([model.iterations+1, ], dtype=float)
+    WM  = np.empty([model.iterations+1, ], dtype=float)
     z   = np.empty([model.iterations+1, model.z_size], dtype=float)
     u   = np.empty([model.iterations+1, model.inner_points+2], dtype=float)
     z0  = np.empty([model.z_size, ], float)
@@ -202,10 +138,16 @@ def solve(model):
              u[0, :] = h2u(z0[model.first_idx: model.last_idx+1],
                            model.n, model.m, model.gamma)
 
-             GC[0] = characteristics(t[0], u[0, :], mass_in, mass_out,
-                                     s1, s2, model, chtype='gc')[0]
+             WM_total, WM_in_tube = water_mass(u[0, :], mass_in, mass_out,
+                                               s1, s2, model)
+             WM[0] = WM_total
+             GC[0] = calc_gc(u[0, :], mass_in, mass_out, s1, s2, WM_in_tube,
+                             model)
         else:
             z0 = z[i-1, :]
+
+        print(z)
+        input('press')
 
         (flag, t_out, z[i, :]) = simulate_direct(model, residual_fn, z0)
 
@@ -218,8 +160,15 @@ def solve(model):
 
         mass_in  = z[i, model.mass_in_idx]
         mass_out = 0.0 # for GC and RM no expelled water is taken into account
-        GC[i] = characteristics(t_out, u[i, :], mass_in, mass_out,
-                                          s1, s2, model, chtype='gc')[0]
+        WM_total, WM_in_tube = water_mass(u[i, :], mass_in, mass_out,
+                                          s1, s2, model)
+        WM[i] = WM_total
+        GC[i] = calc_gc(u[i, :], mass_in, mass_out, s1, s2, WM_in_tube,
+                        model)
+
+        # print('results', GC, z[:, model.mass_out_idx], u,
+        #       z[:, :model.last_idx+1])
+
 
     if model.draw_graphs:
         from modules.shared.show import draw_graphs
@@ -228,6 +177,6 @@ def solve(model):
         GC[:] = 0.5
 
         draw_graphs(t, y=model.y, h=h, u=u, mass_out=z[:, model.mass_out_idx],
-                    GC=GC, s1=z[:, model.s1_idx], s2=z[:, model.s2_idx])
+                    GC=GC, WM=WM, s1=z[:, model.s1_idx], s2=z[:, model.s2_idx])
 
     return (flag, t, z, GC)
