7e5d82079a286800551971dcd0e920cee3d3abff
Pavol Kišon <pavol.kison@gmail.com>
2012-04-24 17:07:55 +0200

    ids: add measured data 'wl_out1', 'gc1' and 'rm1' to optional options and verify that at least one of them is specified.

--- modules/inverse_draining_saturated/options.py
+++ modules/inverse_draining_saturated/options.py
@@ -1,12 +1,12 @@
 PARENTAL_MODULES = ['direct_draining_saturated']
 
 CONFIG_OPTIONS = {
-        'mandatory' : ['inv_init_params', 'gc1', 'wl_out1'],
+        'mandatory' : ['inv_init_params'],
         'defaults'  : {},
         'dependent' : {'inv_params':
                        (lambda cfg: len(cfg.get_value('inv_init_params')) == 2,
                         ['ks'])},
-        'optional'  : [],
+        'optional'  : ['wl_out1', 'gc1', 'rm1'],
         'additional': []
         }
 
@@ -21,6 +21,13 @@ def check_cfg(cfg):
     if not ((inv_params_len == 3) or (inv_params_len == 2)):
         print('Option ''inv_params'' should be of length 3 or 2.')
         return False
+
+    if ((cfg.get_value('wl_out1') is None) and (cfg.get_value('gc1') is None)
+        and (cfg.get_value('rm1') is None)):
+        print('No measured data (wl_out1, gc1, rm1) is specified. Cannot run '
+              'inverse problem')
+        return False
+
     return True
 
 def adjust_cfg(cfg):
