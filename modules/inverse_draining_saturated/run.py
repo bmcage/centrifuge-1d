fade248e034510e857459d16951ea34f25aa3057
Pavol Kišon <pavol.kison@gmail.com>
2012-04-30 14:06:18 +0200

    modules.inverse_draining_saturated: - for print_results() fix calling of numpy functions, as numpy is not anymore imported globaly - solve() - adapt to the new version of simulate_inverse()

--- modules/inverse_draining_saturated/run.py
+++ modules/inverse_draining_saturated/run.py
@@ -1,7 +1,7 @@
 from modules.direct_draining_saturated.run import solve as solve_direct
 from modules.shared.solver import simulate_inverse
 from modules.shared.shared_functions import scale_array
-from numpy import concatenate, asarray, empty
+from numpy import concatenate, asarray, empty, zeros, ones
 
 def print_results(model, inv_params, t_inv, wl_out1_inv, gc1_inv):
     duration = model.get_iterable_value('duration')
@@ -9,23 +9,23 @@ def print_results(model, inv_params, t_inv, wl_out1_inv, gc1_inv):
     subexperiments = len(duration)
 
     if model.calc_wl_out:
-        wl_out1  = np.asarray(model.get_iterable_value('wl_out1'))
+        wl_out1  = asarray(model.get_iterable_value('wl_out1'))
         wl_out1[wl_out1 == 0.0] = 1.0e-10
         print('WL_out_measured: ', subexperiments *  '% 8.6f'
               % tuple(wl_out1))
         print('WL_out_computed: ', subexperiments *  '% 8.6f'
               % tuple(wl_out1_inv))
         print('Error (\%):  ', subexperiments * '    % 5.2f'
-              % tuple((gc1_inv - gc1) / gc1 * 100.))
+              % tuple((wl_out1_inv - wl_out1) / wl_out1 * 100.))
     if model.calc_gc:
-        gc1  = np.asarray(model.get_iterable_value('gc1'), dtype=float)
+        gc1  = asarray(model.get_iterable_value('gc1'), dtype=float)
         print('GC_measured: ', subexperiments *  '% 9.6f' % tuple(gc1))
         print('GC_computed: ', subexperiments *  '% 9.6f' % tuple(gc1_inv))
         print('Error (\%):  ', subexperiments * '    % 5.2f'
               % tuple((gc1_inv - gc1) / gc1 * 100.))
 
     if model.calc_rm:
-        rm1  = np.asarray(model.get_iterable_value('rm1'), dtype=float)
+        rm1  = asarray(model.get_iterable_value('rm1'), dtype=float)
         print('RM_measured: ', subexperiments *  '% 9.6f' % tuple(rm1))
         print('RM_computed: ', subexperiments *  '% 9.6f' % tuple(rm1_inv))
         print('Error (\%):  ', subexperiments * '    % 5.2f'
@@ -48,6 +48,7 @@ def solve(model):
             (model.ks, model.n, model.gamma) = optim_args
         else:
             (model.n, model.gamma) = optim_args
+
         model.m = 1-1/model.n
 
         (_flag, t, z, gc1, rm1) = solve_direct(model)
@@ -63,17 +64,10 @@ def solve(model):
         if model.calc_rm:
             rm1 = rm1[1:]
 
-        print('\noptimization parameters: ', optim_args)
         print('gc_mes, wl_mes: t_exp', model.get_iterable_value('gc1'),
               model.get_iterable_value('wl_out1'),
               model.get_iterable_value('duration'))
         print('gc_com, wl_com: t_com', gc1[1:], wl_out1[1:], t)
-
-        return (t, wl_out1, gc1, rm1)
-
-    def lsq_ip_direct_drainage(model, *optim_args):
-
-        (t, wl_out1, gc1, rm1) = ip_direct_drainage(model, optim_args)
 
         if model.calc_wl_out:
             scale_array(wl_out1, wl_out1)
@@ -82,7 +76,7 @@ def solve(model):
         if model.calc_rm:
             scale_array(rm1, rm1)
 
-        result = concatenate((wl_out1, gc1, rm1))
+        return (t, wl_out1, gc1, rm1)
 
         return result
 
@@ -109,16 +103,14 @@ def solve(model):
 
         data_measured = concatenate((wl_out_meas, gc_meas, rm_meas))
 
-        lsq_direct_fn = lsq_ip_direct_drainage
-        direct_fn     = ip_direct_drainage
-
     xdata         = model
     init_params   = model.inv_init_params
 
-    inv_params, cov_ks = simulate_inverse(lsq_direct_fn, xdata, data_measured,
-                                          init_params)
+    inv_params, cov_ks = simulate_inverse(ip_direct_drainage, xdata,
+                                          data_measured, init_params)
 
-    (t_inv, wl_out1_inv, gc1_inv, rm1_inv) = direct_fn(xdata, inv_params)
+    (t_inv, wl_out1_inv, gc1_inv, rm1_inv) = ip_direct_drainage(xdata,
+                                                                inv_params)
 
     print_results(model, inv_params, t_inv, wl_out1_inv, gc1_inv)
 
