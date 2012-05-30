647ccf791b479a09f1a5f4b3652ef53376ea05b7
Pavol Kišon <pavol.kison@gmail.com>
2012-05-30 11:00:53 +0200

    dds: - rename ResFunction -> IDA_Rhsfunction (due to changes in sundials interface) - add s2 inteface between the partially saturated and saturated part   (we have outflow and moving interface) - this experiments correspond   to a case when the sample is partially dipped in water - in solve() move the initialization stuff outside the next_iteration() cycle   and fix time 't' as it should reflect the returned value (which can   in general include the falling-head simulation as well)

--- modules/direct_draining_saturated/run.py
+++ modules/direct_draining_saturated/run.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-from scikits.odes.sundials.common_defs import ResFunction
+from scikits.odes.sundials.common_defs import IDA_RhsFunction
 from modules.shared.shared_functions import find_omega2g
 from modules.shared.vangenuchten import h2Kh, dudh, h2u
 from modules.shared.characteristics import water_mass, calc_gc, calc_rm
 from modules.shared.solver import simulate_direct
 
-class centrifuge_residual(ResFunction):
+class centrifuge_residual(IDA_RhsFunction):
 
     def evaluate(self, t, z, zdot, result, model):
         # F(t,h,dh/dt) = porosity * du/dh * dh/dt
@@ -26,7 +26,7 @@ class centrifuge_residual(ResFunction):
 
         omega2g = find_omega2g(t, model)
 
-        s2 = model.l0
+        s2 = z[model.s2_idx]
         s1 = 0
         ds = s2 - s1
 
@@ -61,32 +61,39 @@ class centrifuge_residual(ResFunction):
                                         + 2 / (dy[:-1] + dy[1:]) / ds
                                           * (q12[1:] - q12[:-1]))
 
-        if model.rb_type == 0:
-            q_last = 0.
-            result[last_idx]  = (porosity * du_dh[-1] * hdot[-1]
-                                 + 2 / dy[-1] / ds * (q_last - q12[-1]))
-        elif model.rb_type == 1:
-            dhdr_last = (model.ldc1[-1] * h[-3]
-                         - model.ldc2[-1] * h[-2]
-                         + model.ldc3[-1] * h[-1])
-            Kh_last =  h2Kh(h[-1], n, m, gamma, Ks)
-            q_last  = np.maximum(1e-12,
-                                 -Kh_last * (dhdr_last/ds - omega2g*(r0 + L)))
-            result[last_idx]  = (porosity * du_dh[-1] * hdot[-1]
-                                 + 2 / dy[-1] / ds * (q_last - q12[-1]))
+        rb_type = model.rb_type
+        dhdr_last = (model.ldc1[-1] * h[-3]
+                     - model.ldc2[-1] * h[-2]
+                     + model.ldc3[-1] * h[-1])
+
+        if rb_type > 3:
+            dhdotdr_last = (model.ldc1[-1] * hdot[-3]
+                             - model.ldc2[-1] * hdot[-2]
+                             + model.ldc3[-1] * hdot[-1])
+
+            result[model.s2_idx]  = zdot[model.s2_idx] + dhdotdr_last/dhdr_last
         else:
-            dhdr_last = (model.ldc1[-1] * h[-3]
-                         - model.ldc2[-1] * h[-2]
-                         + model.ldc3[-1] * h[-1])
-            Kh_last =  h2Kh(h[-1], n, m, gamma, Ks)
-            q_last  = np.maximum(1e-12,
-                                 -Kh_last * (dhdr_last/ds - omega2g*(r0 + L)))
-            result[last_idx]  = hdot[-1]
+            if rb_type == 0:
+                q_last = 0.
+                result[last_idx]  = (porosity * du_dh[-1] * hdot[-1]
+                                     + 2 / dy[-1] / ds * (q_last - q12[-1]))
+            elif rb_type == 1:
+                Kh_last =  h2Kh(h[-1], n, m, gamma, Ks)
+                q_last  = np.maximum(1e-12,
+                                     -Kh_last * (dhdr_last/ds - omega2g*(r0 + L)))
+                result[last_idx]  = (porosity * du_dh[-1] * hdot[-1]
+                                     + 2 / dy[-1] / ds * (q_last - q12[-1]))
+            else:
+                Kh_last =  h2Kh(h[-1], n, m, gamma, Ks)
+                q_last  = np.maximum(1e-12,
+                                     -Kh_last * (dhdr_last/ds - omega2g*(r0 + L)))
+                result[last_idx]  = hdot[-1]
+
+            result[model.s2_idx]  = zdot[model.s2_idx]
 
         result[model.mass_in_idx]  = zdot[model.mass_in_idx]
         result[model.mass_out_idx] = zdot[model.mass_out_idx]  - q_last
         result[model.s1_idx]  = zdot[model.s1_idx]
-        result[model.s2_idx]  = zdot[model.s2_idx]
         result[model.pq_idx]  = zdot[model.pq_idx]
 
         return 0
@@ -104,57 +111,57 @@ def solve(model):
 
     model.init_iteration()
 
+    # initialization:
+    # set values for t[0], z[0], u[0], GC[0]
+    z0 = z[0, :]
+
+    z0[model.first_idx:model.last_idx+1] = model.h_init
+    if model.rb_type == 2:
+        # do regularization for prescribed head on right boundary
+        n_spanning_points = 5
+
+        z0_view = z0[model.last_idx-n_spanning_points:model.last_idx+1]
+        y_view = model.y[-1-n_spanning_points:]
+        a = ((model.h_last - model.h_init)
+             / (y_view[-1] - y_view[0]) ** 2)
+
+        z0_view[:] = ((a * np.power(y_view - y_view[0], 2))
+                      + model.h_init)
+        z0_view[-1] = model.h_last
+
+        s1 = 0.0
+        s2 = model.l0
+        mass_in = mass_out = 0.0
+
+        z0[model.s1_idx] = s1
+        z0[model.s2_idx] = s2
+        z0[model.mass_in_idx]  = mass_in
+        z0[model.mass_out_idx] = mass_out
+
+    t[0] = 0.0
+    u[0, :] = h2u(z0[model.first_idx: model.last_idx+1],
+                  model.n, model.m, model.gamma)
+
+    WM_total, WM_in_tube = water_mass(u[0, :], mass_in, mass_out,
+                                      s1, s2, model)
+    WM[0] = WM_total
+    if model.calc_gc:
+        GC    = np.empty([model.iterations+1, ], dtype=float)
+        GC[0] = calc_gc(u[0, :], mass_in, s1, s2, WM_in_tube,
+                                 model)
+    if model.calc_rm:
+        RM    = np.empty([model.iterations+1, ], dtype=float)
+        RM[0] = calc_rm(t[0], u[0, :], mass_in, mass_out, s1, s2,
+                        model)
+
     while model.next_iteration():
         i = model.iteration
 
-        if i == 1:
-             # initialize: z0
-             # set values for t[0], z[0], u[0], GC[0]
-             z0[model.first_idx:model.last_idx+1] = model.h_init
-             if model.rb_type == 2:
-                 # do regularization for prescribed head on right boundary
-                 n_spanning_points = 5
-
-                 z0_view = z0[model.last_idx-n_spanning_points:model.last_idx+1]
-                 y_view = model.y[-1-n_spanning_points:]
-                 a = ((model.h_last - model.h_init)
-                      / (y_view[-1] - y_view[0]) ** 2)
-
-                 z0_view[:] = ((a * np.power(y_view - y_view[0], 2))
-                                + model.h_init)
-                 z0_view[-1] = model.h_last
-
-             s1 = 0.0
-             s2 = model.l0
-             mass_in = mass_out = 0.0
-
-             z0[model.s1_idx] = s1
-             z0[model.s2_idx] = s2
-             z0[model.mass_in_idx]  = mass_in
-             z0[model.mass_out_idx] = mass_out
-
-             t[0] = 0.0
-             z[0, :] = z0
-             u[0, :] = h2u(z0[model.first_idx: model.last_idx+1],
-                           model.n, model.m, model.gamma)
-
-             WM_total, WM_in_tube = water_mass(u[0, :], mass_in, mass_out,
-                                               s1, s2, model)
-             WM[0] = WM_total
-             if model.calc_gc:
-                 GC    = np.empty([model.iterations+1, ], dtype=float)
-                 GC[0] = calc_gc(u[0, :], mass_in, s1, s2, WM_in_tube,
-                                 model)
-             if model.calc_rm:
-                 RM    = np.empty([model.iterations+1, ], dtype=float)
-                 RM[0] = calc_rm(t[0], u[0, :], mass_in, mass_out, s1, s2,
-                                 model)
-        else:
-            z0 = z[i-1, :]
+        z0 = z[i-1, :]
 
         (flag, t_out, z[i, :]) = simulate_direct(model, residual_fn, z0)
 
-        t[i] = t[i-1] + model.duration
+        t[i] = t[i-1] + t_out
 
         u[i, :] = h2u(z[i, model.first_idx: model.last_idx+1],
                       model.n, model.m, model.gamma)
