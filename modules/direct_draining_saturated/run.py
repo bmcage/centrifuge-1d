f393b9b435fba4b87a0783fd8fd22ee5e86a1dde
Pavol Kišon <pavol.kison@gmail.com>
2012-08-27 11:59:46 +0200

    implement new direct_draining_saturated() solver

--- modules/direct_draining_saturated/run.py
+++ modules/direct_draining_saturated/run.py
@@ -167,159 +167,120 @@ class centrifuge_residual(IDA_RhsFunction):
 residual_fn = centrifuge_residual()
 
 def solve(model):
-
-    t   = np.empty([model.iterations+1, ], dtype=float)
-
-    WM  = np.empty([model.iterations+1, ], dtype=float)
-    z   = np.empty([model.iterations+1, model.z_size], dtype=float)
-    u   = np.empty([model.iterations+1, model.inner_points+2], dtype=float)
-    z0  = np.empty([model.z_size, ], float)
-
-    model.init_iteration()
-
-    # initialization:
-    # set values for t[0], z[0], u[0], GC[0]
-    z0 = z[0, :]
-
-    if model.rb_type > 3:
-        atol_orig = model.atol
-        atol = atol_orig * np.ones([model.z_size,], dtype=float)
-        atol[model.s2_idx] = 1e-8
-
-        rtol_orig = model.rtol
-        rtol = rtol_orig * np.ones([model.z_size,], dtype=float)
-        rtol[model.s2_idx] = 1e-4
-
-    z0[model.first_idx:model.last_idx+1] = model.h_init
-    if model.rb_type in [2, 3]:
-        # do regularization for prescribed head on right boundary
-        n_spanning_points = 15
-
-        z0_view = z0[model.last_idx-n_spanning_points:model.last_idx+1]
-        y_view = model.y[-1-n_spanning_points:]
-        a = ((model.h_last - model.h_init)
-             / (y_view[-1] - y_view[0]) ** 2)
-
-        z0_view[:] = ((a * np.power(y_view - y_view[0], 2))
-                      + model.h_init)
-        z0_view[-1] = model.h_last
-
-    if model.rb_type < 3:
-        s2 = model.l0
-    else:
+    # define s1, s2, mass_in, mass_out, u0, wm0, wm_in_tube0
+    if model.rb_type == 3:
         if model.s2_0:
             s2 = model.s2_0
         else:
             s2 = model.l0 - model.dip_height
-
-    if model.rb_type == 3:
-        algvars_idx = [model.s2_idx]
     else:
-        algvars_idx = None
+        s2 = model.l0
 
     s1 = 0.0
     mass_in = mass_out = 0.0
 
-    z0[model.s1_idx] = s1
-    z0[model.s2_idx] = s2
-    z0[model.mass_in_idx]  = mass_in
-    z0[model.mass_out_idx] = mass_out
+    wm0 = wm_in_tube0 = 0.0
 
-    t[0] = 0.0
-    u[0, :] = h2u(z0[model.first_idx: model.last_idx+1],
-                  model.n, model.m, model.gamma)
+    # z0 inicialization
+    def initialize_z0(z0, model):
+        z0[model.first_idx:model.last_idx+1] = model.h_init
 
-    WM_total, WM_in_tube = water_mass(u[0, :], mass_in, mass_out,
-                                      s1, s2, model)
-    WM[0] = WM_total
-    model.wm0 = WM_total
+        if model.rb_type in [2, 3]:
+            # do regularization for prescribed head on right boundary
+            n_spanning_points = 15
+
+            z0_view = z0[model.last_idx-n_spanning_points:model.last_idx+1]
+            y_view = model.y[-1-n_spanning_points:]
+            a = ((model.h_last - model.h_init)
+                 / (y_view[-1] - y_view[0]) ** 2)
+
+            z0_view[:] = ((a * np.power(y_view - y_view[0], 2))
+                      + model.h_init)
+            z0_view[-1] = model.h_last
+
+        z0[model.s1_idx] = s1
+        z0[model.s2_idx] = s2
+        z0[model.mass_in_idx]  = mass_in
+        z0[model.mass_out_idx] = mass_out
+
+        # assign value to u0, WM0 and wm0 (wm0 is needed for mass balance)
+        u0 = h2u(z0[model.first_idx: model.last_idx+1],
+                 model.n, model.m, model.gamma)
+        (wm0, wm_in_tube0) = water_mass(u0, mass_in, mass_out, s1, s2, model)
+        model.wm0 = wm0
+
+    # Initialization
+    if model.rb_type == 3:
+        atol_backup        = model.atol # backup value
+        atol               = atol_backup * np.ones([model.z_size,], dtype=float)
+        atol[model.s2_idx] = model.s2_atol
+        model.atol         = atol
+
+        rtol_backup        = model.rtol # backup value
+        rtol               = rtol_backup * np.ones([model.z_size,], dtype=float)
+        rtol[model.s2_idx] = model.s2_rtol
+        model.rtol         = rtol
+
+        algvars_idx = [model.s2_idx]
+    else:
+        algvars_idx = None
+
+    # Computation
+    (flag, t, z) = simulate_direct_new(initialize_z0, model, residual_fn,
+                                       root_fn = None, nr_rootfns=None,
+                                       algvars_idx=algvars_idx)
+
+    # Restore modified values
+    model.atol = atol_backup
+    model.rtol = rtol_backup
+
+    # Results
+    k  = t.shape
+
+    s1 = z[:, model.s1_idx]
+    s2 = z[:, model.s2_idx]
+    mass_in  = z[:, model.mass_in_idx]
+    mass_out = z[:, model.mass_out_idx]
+
+    h  = z[model.first_idx: model.last_idx+1, :]
+    u  = np.empty([k, model.inner_points+2], dtype=float)
+    WM = np.empty(t.shape, dtype=float)
+    WM_in_tube = np.empty(t.shape, dtype=float)
+
+    u[0, :] = u0
+    WM[0]   = wm0
+    WM_in_tube[0] = wm_in_tube0
+    for i in range(1, k):
+        u[i, :] = h2u(h[i, :], model.n, model.m, model.gamma)
+
+        (wm_total, wm_in_tube) = water_mass(u[i, :], mass_in[i], mass_out[i],
+                                            s1[i], s2[i], model)
+        WM[i]         = wm_total
+        WM_in_tube[i] = wm_in_tube
+
+    no_measurements = np.empty([], dtype=float)
 
     if model.calc_gc:
-        GC    = np.empty([model.iterations+1, ], dtype=float)
-        GC[0] = calc_gc(u[0, :], mass_in, s1, s2, WM_in_tube,
-                                 model)
-    if model.calc_rm:
-        RM    = np.empty([model.iterations+1, ], dtype=float)
-        RM[0] = calc_rm(t[0], u[0, :], mass_in, mass_out, s1, s2,
-                        model)
+        GC = np.empty(t.shape, dtype=float)
 
-    direct_simulator = DirectSimulator(model, residual_fn, algvars_idx)
-
-    while model.next_iteration():
-        i = model.iteration
-
-        z0 = z[i-1, :]
-
-        (flag, t_out) = direct_simulator.run(model.duration, model.fh_duration,
-                                             z0, z[i, :])
-
-        if not flag:
-            if model.calc_gc:
-                GC_r = GC[:i]
-            else:
-                GC_r = None
-
-            if model.calc_rm:
-                RM_r = RM[:i]
-            else:
-                RM_r = None
-            return (flag, t[:i], z[:i, :], GC_r, RM_r)
-
-        t[i] = t_out
-
-        u[i, :] = h2u(z[i, model.first_idx: model.last_idx+1],
-                      model.n, model.m, model.gamma)
-        s1 = z[i, model.s1_idx]
-        s2 = z[i, model.s2_idx]
-
-        mass_in  = z[i, model.mass_in_idx]
-        mass_out = z[i, model.mass_out_idx]
-        WM_total, WM_in_tube = water_mass(u[i, :], mass_in, mass_out,
-                                          s1, s2, model)
-        WM[i] = WM_total
-        if model.calc_gc:
-            GC[i] = calc_gc(u[i, :], mass_in, s1, s2, WM_in_tube,
+        for i in range(k):
+            GC[i] = calc_gc(u[i, :], mass_in[i], s1[i], s2[i], WM_in_tube[i],
                             model)
+    else:
+        GC = no_measurements
 
-        if model.calc_rm:
-            RM[i] = calc_rm(t[i], u[i, :], mass_in, mass_out, s1, s2, model)
+    if model.calc_rm:
+        RM = np.empty(t.shape, dtype=float)
 
-        # print('results', GC, z[:, model.mass_out_idx], u,
-        #       z[:, :model.last_idx+1])
-    print()
+        for i in range(k):
+            RM[i] = calc_rm(t[i], u[i, :], mass_in[i], s1[i], s2[i], model)
+    else:
+        RM = no_measurements
 
-    if model.rb_type > 3:
-        model.atol = atol_orig
-        model.rtol = rtol_orig
-
-    if model.draw_graphs:
-        from modules.shared.show import draw_graphs
-
-        h = z[:, model.first_idx:model.last_idx+1]
-
-        if not model.calc_gc:
-            GC = None
-
-        if not model.calc_rm:
-            RM = None
-
-        draw_graphs(t, y=model.y, h=h, u=u, mass_out=z[:, model.mass_out_idx],
-                    GC=GC, RM=RM, WM=WM,
-                    s1=z[:, model.s1_idx], s2=z[:, model.s2_idx],
-                    save_figures=model.save_figures,
-                    separate_figures=model.separate_figures,
-                    save_as_text=model.save_as_text,
-                    model=model)
-
-    if not model.calc_gc:
-        GC = np.asarray([], dtype=float)
-    if not model.calc_rm:
-        RM = np.asarray([], dtype=float)
-
-    return (flag, t, z, GC, RM)
+    return (flag, t, z, GC, RM, u, WM, WM_in_tube)
 
 def run(model):
-    (flag, t, z, GC, RM) = solve(model)
+    (flag, t, z, GC, RM, u, WM, WM_in_tube) = solve(model)
 
     if not flag:
         print("Solver could not compute the solution... Exiting...")
