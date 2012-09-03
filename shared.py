34e99195706bf6602d4b7e3d906c2961682fc099
Pavol Kišon <pavol.kison@gmail.com>
2012-09-03 13:03:42 +0200

    shared.py: add load_configuration() function that loads config file with all defaults and mask

--- shared.py
+++ shared.py
@@ -19,10 +19,42 @@ def load_configuration_file(basedir, *inifilenames):
     else:
         return None
 
+def load_configuration(exp_id, exp_no, tube_no, mask=None):
+    base_dir     = INI_DIR + '/'
+    exp_base_dir = base_dir + exp_id + '/'
+    exp_dir      = exp_base_dir + str(exp_no) + '/'
+    tube_dir     = exp_dir + 'tube' + str(tube_no) + '/'
+    masks_dir    = tube_dir + MASKS_DIRNAME + '/'
 
+    search_dirs  = (base_dir, exp_base_dir, exp_dir, tube_dir)
 
+    defaults_files = list(filter(lambda fname: exists(fname),
+                                 map(lambda cfgdir: cfgdir + DEFAULTS_ININAME,
+                                     search_dirs)))
+
+    # Measurements: 1. list filenames 2. remove 'defaults.ini' and 'mask' dir
+    #               3. prefix filenames with path
+    measurements_filenames = listdir(tube_dir)
     for fname in (DEFAULTS_ININAME, MASKS_DIRNAME):
+        if fname in measurements_filenames:
+            measurements_filenames.remove(fname)
+    measurements_files = list(map(lambda fname: tube_dir + fname,
+                                  measurements_filenames))
 
+    if mask:
+        mask_filename = masks_dir + mask + '.ini'
+        if not exists(mask_filename):
+            print('Mask file "{}" does not exist in expected location:'
+                  '\n{}.'.format(mask, masks_dir))
+            if yn_prompt('Do you wish to continue without applying '
+                         'the mask? [Y/n]: '):
+                mask_filename = None
+            else:
+                exit(0)
+
+    cfg_files = defaults_files + measurements_files + [mask_filename]
+
+    return Configuration().read_from_files(*cfg_files)
 
 def print_by_tube(tube_number, tube_data):
     print('Tube number: ', tube_number)
