015248dead8ee198b20b7156ec19879eeab5f95a
Pavol Kišon <pavol.kison@gmail.com>
2012-09-15 09:04:13 +0200

    remove comments

--- modules/shared/show.py
+++ modules/shared/show.py
@@ -126,12 +126,6 @@ DG_AXES_LABELS = {'h': (dg_label_length, "Piezometric head $h$ [cm]"),
                   's2': (dg_label_time, "Interface s2 [cm]"),
                   'WM': (dg_label_time, "Water mass [cm]"),
                   'RC': ("Water content $\\theta$", "Pressure $p$ [Pa]")}
-              # '': (dg_label_time, ""),
-              # '': (dg_label_time, ""),
-              # '': (dg_label_time, ""),
-              # '': (dg_label_time, ""),
-              # '': (dg_label_time, ""),
-
 DG_PAIRS = (('h', 'u'), ('MI', 'MO'), ('GC', 'RM'), ('s1', 's2'))
 
 def mk_plot_item(xdata, ydata, label=None, line_opts=None):
