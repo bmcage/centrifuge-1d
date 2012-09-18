736a340a301d0a2dc0509aa30c3abadd2cdb2711
Pavol Kišon <pavol.kison@gmail.com>
2012-09-18 11:07:31 +0200

    dds: add discretiazation of x-axis where dx(i)=k*dx(i-1) with k being supplied by configuration

--- modules/direct_draining_saturated/options.py
+++ modules/direct_draining_saturated/options.py
@@ -1,5 +1,13 @@
 from modules.shared.functions import lagrangean_derivative_coefs
-from numpy import linspace, power, asarray
+from numpy import linspace, power, asarray, empty
+
+def dtype_deps(cfg):
+    dtype = cfg.get_value('dtype')
+    result = []
+    if dtype == 1: pass
+    elif dtype == 2: result = ['k_dx']
+
+    return result
 
 PARENTAL_MODULES = ['base']
 
@@ -7,6 +15,7 @@ CONFIG_OPTIONS = ['inner_points', 'dtype', 'n', 'gamma',
                   'h_init', 'porosity',
                   'calc_gc', 'calc_rm', 'calc_wm',
                   'rb_type',
+                  dtype_deps,
                   # dependent
                   (lambda cfg: cfg.get_value('rb_type') == 2,
                     ['h_last']),
@@ -57,11 +66,22 @@ def adjust_cfg(cfg):
     cfg.set_value('z_size',       inner_points+7)
 
     discretization_type = cfg.get_value('dtype')
-    if discretization_type == 1:
+    if discretization_type == 1:   # linear discretization
         y = linspace(0, 1, inner_points + 2)
+    elif discretization_type == 2: # L= a+ka+(k^2)a+...+(k^inner_points)a
+        # L=1 (as we use transformed interval <0,1>)
+        # L = a*[(1-k^(inner_points +1))/(1-k)]
+        k = cfg.get_value('k_dx')
+        a=(1-k)/(1-power(k, inner_points+1))
+        y= empty([inner_points+2, ])
+        y[0] = 0.0; y[-1] = 1.0
+        for i in range(1, inner_points+1):
+            y[i] = y[i-1] + a
+            a = a*k
     else:
-        raise NotImplementedError('For now only linear discretization is'
-                                  ' implemented')
+        print('Unsupported discretization type:', dtype)
+        exit(1)
+
 
     cfg.set_value('y', y)
     cfg.set_value('y12', (y[1:]+y[:-1])/2.)
