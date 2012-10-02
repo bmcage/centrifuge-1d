8e8864bf7183743f2a7c64eb1e05646f2ae3b4df
Pavol Kišon <pavol.kison@gmail.com>
2012-10-02 16:08:37 +0200

    modules.shared.functions: add show_results() general function that takes care of displaying results

--- modules/shared/functions.py
+++ modules/shared/functions.py
@@ -121,3 +121,27 @@ def y2x(y, s1, s2):
         x[i, :] = s1[i] + y * ds[i]
 
     return x
+
+def show_results(extract_data, model, inv_params=None, cov=None):
+    from modules.shared.functions import measurements_time
+    from modules.shared.show import ResultsData, DPlots
+
+    t_meas = measurements_time(model)[1:]
+    if model.wl_out:
+        wl_out = np.cumsum(np.asarray(model.wl_out, dtype=float))
+    else:
+        wl_out = None
+    measurements = {'MI': (t_meas, model.wl1), 'MO': (t_meas, wl_out),
+                    'GC': (t_meas, model.gc1), 'RM': (t_meas, model.rm1)}
+
+    data = ResultsData()
+    data.extract(extract_data, model, model.params_ref,
+                 measurements=measurements)
+    data.add_value(inv_params=inv_params, cov=cov)
+
+    if model.save_data:
+        data.dump(model.experiment_info)
+
+    if model.show_figures:
+        dplots = DPlots(data, model.experiment_info)
+        dplots.display()
