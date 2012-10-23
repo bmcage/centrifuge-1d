60a79653457327ce20f74440619df2a80cb6e417
Pavol Kišon <pavol.kison@gmail.com>
2012-10-23 11:32:28 +0200

    dds options: update documentation

--- modules/direct_draining_saturated/options.desc.py
+++ modules/direct_draining_saturated/options.desc.py
@@ -1,10 +1,33 @@
 CONFIG_OPTIONS = {
-    'set-up': \
-        {'rb_type': ("(required) Right boundary type.\n"
-                     "\t 0 - no outflow [q_last = 0]"
-                     "\t 1 - free outflow"
-                     "\t 2 - prescribed pressure (see h_last)"),
-         'h_last': ("(dependent) The prescribed pressure on the right"
-                    " boundary\n\t if 'rb_type' = 2")
+    'soil': \
+        {'porosity': "Porosity of the soil sample. Type: float",
+         'n': ("Van Genuchten parameter 'n' of the soil. "
+               "Type: float or list"),
+               'gamma': ("Van Genuchten parameter 'gamma' of the soil. "
+                         "Type: float or list")},
+    'initial-and-boundary-conditions': \
+        {'h_init': ("Initial fluid head value (close to 0) for the "
+                    "(almost fully saturated) sample.")
+                    " soil sample (e.g. end of the starting-filter)."
+                    "Type: float or list"),
+         'rb_type': ("Right boundary condition of type:"
+                     "\n\t 0 - no outflow [q_last = 0]"
+                     "\n\t 1 - free outflow"
+                     "\n\t 2 - prescribed pressure (see h_last)"
+                     "\nType: integer"),
+         'h_last': ("The prescribed pressure on the right boundary - only for "
+                    "'rb_type' = 2")
+     },
+    'solver': \
+        {'inner_points': ("How many (inner) point will be used for the sample "
+                          "discretization. Type: integer"),
+         'dtype': ("Discretization type. Determines how the soil sample "
+                   "will be discretized in space. Currently implemented are: "
+                   "\n\t 1 - linear discretization"
+                   "\n\t 2 - the distance of two points is 'k_dx'*(dist of "
+                   "previous two points. See also: k_dx)"
+                   "\nType: integer"),
+
+
         }
     }
