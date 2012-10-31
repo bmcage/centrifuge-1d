4a1b8086bf545ffd09ee44e896094bcc3394a434
Pavol Kišon <pavol.kison@gmail.com>
2012-10-31 14:24:36 +0100

    mostly revert patch and just fix :D

--- modules/shared/functions.py
+++ modules/shared/functions.py
@@ -128,11 +128,13 @@ def has_data(x):
 
 def phases_end_times(a_duration, d_duration, g_duration,
                      include_acceleration):
-    if (not include_acceleration) or (a_duration is None):
+    if a_duration is None:
         a_duration = 0.0
     else:
         a_duration = np.asarray(a_duration, dtype=float)
-        if (not d_duration  is None): a_duration += d_duration
+
+    if include_acceleration and (not d_duration  is None):
+        a_duration += d_duration
 
     if g_duration is None:
         g_duration = 0.0
