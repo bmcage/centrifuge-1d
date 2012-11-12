01bc7d475852f7531435b7671dbaea64b09dfe1f
Pavol Kišon <pavol.kison@gmail.com>
2012-11-12 09:23:48 +0100

    remove comments

--- const.py
+++ const.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from os import sep, getcwd
-#from platform import python_version_tuple
 
 #-------------------------------------------------------------------------
-#
 # Paths
-#
 #-------------------------------------------------------------------------
 PROGRAM_DIR = getcwd()
 DATA_DIR    = PROGRAM_DIR + sep + 'data'
@@ -13,7 +10,9 @@ CSV_DIR     = DATA_DIR    + sep + 'csv'
 INI_DIR     = DATA_DIR    + sep + 'datafiles'
 FIGS_DIR    = DATA_DIR    + sep + 'datafiles'
 
-# Default names
+#-------------------------------------------------------------------------
+# Default file and directory names
+#-------------------------------------------------------------------------
 DEFAULTS_ININAME = 'defaults.ini'
 CONSTANTS_ININAME = 'constants.ini'
 PLOTSTYLE_ININAME = 'plotstyles.ini'
@@ -22,16 +21,4 @@ MASKS_DIRNAME    = 'masks'
 MEASUREMENTS_NAMES = {'MI': 'wl1', 'MO': 'wl_out', 'GC': 'gc1', 'RM': 'rm1',
                       'theta': 'theta'}
 #-------------------------------------------------------------------------
-#
-# About box information
-#
 #-------------------------------------------------------------------------
-# URL_HOMEPAGE    = "http://cage.ugent.be/~bm/progs"
-
-# PROGRAM_NAME   = "Centrifuge"
-# VERSION        = "0.1.1a"
-# #COPYRIGHT_MSG  = u"\u00A9 2009 Pavol Kišon" \
-# #                  u"\u00A9 2009 Benny Malengier"
-# COMMENTS       = "Centrifuge is a simulator of an water infiltration "\
-#                  "into soil sample put in a centrifuge"
-# PYTHONVERSION  = python_version_tuple()
