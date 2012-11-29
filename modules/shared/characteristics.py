97f653653eb0dcb81f86009b0d52423d0f0e575d
Pavol Kišon <pavol.kison@gmail.com>
2012-11-29 20:23:47 +0100

    Measurements: remove obsolete functions

--- modules/shared/characteristics.py
+++ modules/shared/characteristics.py
@@ -4,6 +4,7 @@ import numpy as np
 from modules.shared.functions import determine_scaling_factor
 from shared import get_directories, flatten
 from os import makedirs, path
+from modules.shared.vangenuchten import h2u
 
 # MEASUREMENTS_NAMES are the mapping between the internal
 # denotation of measured data (used during computation and for
@@ -196,6 +197,20 @@ class Measurements():
     def iterate_calc_measurements(self):
         for (meas_id, value) in self._computed.items():
             yield (meas_id, value)
+
+    def store_calc_u(self, h, n, m, gamma):
+        meas_id = 'u'
+        if not meas_id in self._computed:
+            self._computed[meas_id] = \
+              np.empty([self._measurements_nr, np.alen(h)], dtype=float)
+            self._indexes[meas_id]  = 0
+
+        value = self._computed[meas_id][self._indexes[meas_id], :] # reference
+
+        h2u(h, n, m, gamma, value)
+        self._indexes[meas_id] += 1
+
+        return value
 
     def store_calc_wm(self, u, dy, s1, s2, mass_in, saturated_length,
                       free_fluid_length, porosity, fl2, fp2, tube_area=1.0,
@@ -399,150 +414,3 @@ def calc_force(u, y, dy, r0, s1, s2, mass_in, d_sat_s, d_sat_e,
         F_sat += fp2 * calc_sat_force(r0+fr2, r0+fr2+fl2, fluid_density)
 
     return F_unsat + F_sat
-
-def water_mass(u, dy, s1, s2, mass_in, saturated_length, free_fluid_length,
-               porosity, fl2, fp2):
-    """
-      Determine the amount of water contained in the experiment.
-      The amount is the sum of water in inflow chamber (mass_in), outflow
-      chamber + any other source like water in basin (free_fluid_length)
-      and saturated zone (saturated_length) just as unsaturated zone
-      (which is between s1 and s2). The soil is characterized by it's
-      porosity (porosity).
-      Also water in filter of length fl2 with porosity fp2 is considered,
-      but only for fully saturated filter.
-    """
-    # Water mass
-    ds = s2 - s1
-    unsat = ds/2  * (dy[0]* u[0] + dy[-1]*u[-1]
-                     + np.sum((dy[:-1] + dy[1:])*u[1:-1]))
-
-    WM_in_tube = (mass_in + porosity*(saturated_length + unsat))
-    WM_in_tube += fp2 * fl2
-
-    WM_total   = WM_in_tube + free_fluid_length
-
-    return WM_total, WM_in_tube
-
-def calc_gc(u, y, dy, s1, s2, mass_in, d_sat_s, d_sat_e, soil_porosity,
-            fl2, fp2, fr2, WM_in_tube, fluid_density, from_end=None):
-    """
-      Determine the gravitational center of water in the sample. The water
-      on the inflow is taken into account (but not water on the outflow).
-      Computed GC is measured from the BEGINNING of the soil sample
-      (in the direction from centrifuge axis)
-
-      Arguments:
-      u - relative saturation
-      y - transformed interval where holds: u(x) = u(fl1 + s1 + (s2-s1)y)
-      dy - difference of y: dy = diff(y) = y(i+1) - y(i)
-      s1, s2 - interfaces (s1 < s2); between them is the unsaturated part
-      mass_in - water in the inflow chamber
-      d_sat_s - distance of the beginning of saturated part from r0
-      d_sat_e - distance of the end of saturated part from r0
-      soil_porosity - porosity of the soil sample
-      fl2, fp2, fr2 - ending filter length, porosity and distance from sample
-                      beginning (to filter's beginning)
-      WM_in_tube - amount of water contained inside the tube
-      fluid_density - density of used fluid (in g/cm3)
-      from_end - (optional) if specified, computed GC will be returned as
-                 distance from the "from_end" point
-    """
-    r0 = 0.0
-
-    gc =  (calc_force(u, y, dy, r0, s1, s2, mass_in, d_sat_s, d_sat_e,
-                      soil_porosity, fl2, fp2, fr2, fluid_density)
-            / WM_in_tube)
-
-    if not from_end is None: gc = from_end - gc
-
-    return gc
-
-def calc_cf_i(omega2g, u, y, dy, r0, s1, s2, mass_in, d_sat_s, d_sat_e,
-            soil_porosity, fl2, fp2, fr2, l0, fluid_density):
-    """
-      Determine the centrifugal force of water in the sample. The water
-      on the inflow is taken into account (but not water on the outflow).
-      Computed centrifugal force is measured from the BEGINNING of the soil
-      mple (in the direction from centrifuge axis)
-
-      Arguments:
-      omega2g - value of (omega^2/g) at given time
-      u - relative saturation
-      y - transformed interval where holds: u(x) = u(fl1 + s1 + (s2-s1)y)
-      dy - difference of y: dy = diff(y) = y(i+1) - y(i)
-      r0 - distance from the centrifuge axis to soil sample beginning
-      s1, s2 - interfaces (s1 < s2); between them is the unsaturated part
-      mass_in - water in the inflow chamber
-      d_sat_s - distance of the beginning of saturated part from r0
-      d_sat_e - distance of the end of saturated part from r0
-      soil_porosity - porosity of the soil sample
-      fl2, fp2, fr2 - ending filter length, porosity and distance from sample
-                      beginning (to filter's beginning)
-    """
-
-    return (omega2g * calc_force(u, y, dy, r0, s1, s2, mass_in, dsat_s, d_sat_e,
-                                 soil_porosity, fl2, fp2, fr2, fluid_density))
-
-def calc_cf_o(omega2g, rS, rE, fluid_density):
-    return (omega2g * calc_sat_force(rS, rE, fluid_density))
-
-def calc_f_mo(omega2g, mo, MO_calibration_curve):
-    calibration = MO_calibration_curve
-
-    (mo0, gc0) = calibration[0]
-
-    if mo < mo0:
-        print('Amount of water: ', mo, ' is less than first point on the '
-              'calibration curve: ', calibration[0],'. Cannot proceed, '
-              'exiting...')
-        exit(1)
-
-    GC = -1.0
-
-    for (mo1, gc1) in calibration[1:]:
-        if mo < mo1:
-            GC = gc0 + (mo - mo0)/(mo1 - mo0) * (gc1 - gc0)
-
-    if GC < 0.0:
-        print('Amount of expelled water: ', mo,  ' is more than the last '
-              'point on the calibration curve: ', calibration[0],'. Cannot '
-              'proceed, exiting...')
-        exit(1)
-
-    F = omega2g * GC * mo
-
-    return F
-
-def calc_rm(t, u, mass_in, mass_out, s1, s2, model):
-
-    raise NotImplementedError('Calculation of rotational momentum is not '
-                              'verified and therefore not provided.')
-
-    porosity = model.porosity
-    y  = model.y
-    dy = model.dy
-    L  = model.l0
-    l0_out = L + model.wt_out
-    l_out  = L + model.wt_out - mass_out
-
-    ds = s2 - s1
-
-    P = np.pi * model.d / 4
-    omega2g = find_omega2g(t, model.omega, model)
-
-    # Rotational momentum
-    r0 = model.r0
-    rm_unsat = (porosity * 1/4 * model.density * ds
-                * (np.power(r0 + s1, 2)*dy[0]*u[0]
-                   + np.power(r0 + s2, 2)*dy[-1]*u[-1]
-                   + np.sum((dy[:-1]+dy[1:]) * u[1:-1]
-                            * np.power(r0 + s1 + ds*y[1:-1], 2))))
-    rm_sat   = (1/6 * model.density
-                * (porosity * (np.power(r0 + s1, 3) - np.power(r0, 3))
-                   + (np.power(r0, 3) - np.power(r0 - mass_in, 3))
-                   + (np.power(r0 + l0_out, 3) - np.power(r0 + l_out, 3))))
-
-    RM = omega2g * P * (rm_unsat + rm_sat)
-
-    return RM
