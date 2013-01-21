7852f750878a3ea10d3ffe083ce2aae5cc2f6a33
Pavol Kišon <pavol.kison@gmail.com>
2013-01-21 12:30:00 +0100

    add 'base-unsaturated' module as a base module for simulation unsaturated flow; this meta class contains base options and functions needed for unsaturated flow

---
+++ modules/base-unsaturated/run.py
@@ -0,0 +1,7 @@
+
+# unsaturated_cfg = {
+#            'fluid': {'s1_0': 0.1, 's2_0': 0.2 },
+#   'discretization': {'inner_points': 80, 'first_point_offset': 80.0,
+#                      'dtype': 1, 'percent_in_saturation': 40.0,
+#                      'approximation_type': 5, 'mb_epsilon': 1e-5}
+
