a4d9ed869f4c5eef049a8be2609735479f131723
Pavol Kišon <pavol.kison@gmail.com>
2013-02-14 02:45:56 +0100

    simplify fit retention curve

--- modules/fit_retention_curve/run.py
+++ modules/fit_retention_curve/run.py
@@ -4,27 +4,21 @@ import numpy as np
 from scipy.optimize import leastsq
 from modules.shared.vangenuchten import h2u, retention_curve
 from modules.shared.show import show_results
+from modules.shared.solver import print_params
 
-def solve(model):
+def solve(model, measurements):
      # list of measured data - we have only one measurement, so first item
-    theta_measured = model.measurements.get_values()[0]
+    theta_measured = measurements.get_values()[0]
 
     def direct_wrapper(optim_args):
-        out = ''
+        params = dict(zip(params_names, optim_args))
+        model.set_parameters(params)
         if model.verbosity > 1:
-            for (name, value) in zip(params_names, optim_args):
-                out += '{}: {: >10.7f}     '.format(name, value)
-            print(out)
+            print_params(params)
 
-        for (name, value) in zip(params_names, optim_args):
-            setattr(model, name, value)
-            if name == 'n':
-                setattr(model, 'm', 1. - 1./value)
-
-        u = h2u(model.h, model.n, model.m, model.gamma)
-
-        (theta_s, theta_r) = (model.theta_s, model.theta_r)
-        theta = theta_r + (theta_s - theta_r) * u
+        (p, theta) = retention_curve(model.n, model.gamma, model.theta_s,
+                                     model.rho, model.g, model.theta_r,
+                                     p=None, h=model.h)
 
         return theta - theta_measured
 
@@ -47,30 +41,14 @@ def solve(model):
                    xtol=model.xtol, ftol=model.ftol,
                    full_output=True)
 
-    optim_params = {key: value
-                    for (key, value) in zip(params_names, opt_values)}
-    s_sq = np.power(direct_wrapper(opt_values),2).sum() / (len(model.h) - len(params_names))
+    optim_params = dict(zip(params_names, opt_values))
+    s_sq = (sum(np.power(direct_wrapper(opt_values), 2))
+            / (len(model.h) - len(params_names)))
 
     return (optim_params, cov * s_sq)
 
-def extract_data(model):
-    (n, gamma, theta_s, theta_r) = (model.n, model.gamma,
-                                    model.theta_s, model.theta_r)
-
-    (p, theta) = retention_curve(n, gamma, theta_s, model.rho,
-                                 model.g, theta_r=theta_r)
-
-    p_meas = np.asarray(model.p, dtype=float)
-    (p_meas, theta_in_measured_points) = \
-      retention_curve(n, gamma, theta_s, model.rho, model.g,
-                      theta_r=theta_r, p=p_meas)
-
-    extracted_data = {'theta': (p, theta, theta_in_measured_points)}
-
-    return (True, extracted_data)
-
 def run(model):
-    (inv_params, cov) = solve(model)
+    (inv_params, cov) = solve(model, model.measurements)
 
     # DISPLAY RESULTS:
     if inv_params:
