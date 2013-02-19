5e0b99acf8aff22b44f53bc237b36ab016d6ed94
Pavol Kišon <pavol.kison@gmail.com>
2013-02-19 14:16:06 +0100

    adapt 'fit_retention_curve' module - we now use the geneneral solve_inverse() function and the 'base_inverse' module

--- modules/fit_retention_curve/run.py
+++ modules/fit_retention_curve/run.py
@@ -1,53 +1,30 @@
 from __future__ import division
 
 import numpy as np
-from scipy.optimize import leastsq
-from modules.shared.vangenuchten import h2u, retention_curve
 from modules.shared.show import show_results
-from modules.shared.solver import print_params
+from modules.shared.solver import simulate_inverse
+
+def solve_direct(model, measurements):
+    # This is not needed when calling simulate_direct() as it resets the
+    # values automatically
+    measurements.reset_calc_measurements() # reset if previously stored values
+
+    measurements.store_calc_theta(model.h, model.SC, model.theta_s,
+                                  model.theta_r, model.rho, model.g)
+
+    return True
 
 def solve(model, measurements):
-     # list of measured data - we have only one measurement, so first item
-    theta_measured = measurements.get_values()[0]
-
-    def direct_wrapper(optim_args):
-        params = dict(zip(params_names, optim_args))
-        model.set_parameters(params)
-        if model.verbosity > 1:
-            print_params(params)
-
-        (p, theta) = retention_curve(model.n, model.gamma, model.theta_s,
-                                     model.rho, model.g, model.theta_r,
-                                     p=None, h=model.h)
-
-        return theta - theta_measured
-
-    #    backup_params = model.get_parameters(model.inv_init_params.keys())
-
     # [p] = Pa = kg/m/s^2 = 10 * g/cm/s^2 -\
     # [h] = cm                            - \
     # => h [cm] =(10*p)/g/rho with [g]=cm/s^2, [rho]=g/cm^3
     p = np.asarray(model.p, dtype=float)
     model.h = -10.*p / model.rho /model.g
 
-    (params_names, init_values) = ([], [])
-    for (name, value) in model.inv_init_params.items():
-        params_names.append(name)
-        init_values.append(value[0])
+    (inv_params, cov) = \
+      simulate_inverse(solve_direct, model, measurements, optimfn=model.optimfn)
 
-    (opt_values, cov, infodic, msg, ier) = \
-          leastsq(direct_wrapper, init_values,
-                   epsfcn=model.epsfcn, factor=model.factor,
-                   xtol=model.xtol, ftol=model.ftol,
-                   full_output=True)
-
-    optim_params = dict(zip(params_names, opt_values))
-    s_sq = (sum(np.power(direct_wrapper(opt_values), 2))
-            / (len(model.h) - len(params_names)))
-
-    return (optim_params, cov * s_sq)
-
-solve_direct = solve
+    return (inv_params, cov)
 
 def run(model):
     (inv_params, cov) = solve(model, model.measurements)
